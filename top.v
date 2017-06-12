838d0996b6634690546fb36d552ffc9192d09c7d
hikalium <hikalium@hikalium.com>
2017-06-12 01:41:18 +0000

    Impl up counter

--- top.v
+++ top.v
@@ -7,13 +7,18 @@ input clk;
 output [7:0] seg;
 output [3:0] segsel;
 
-reg [15:0] data = 16'h1246;
+reg [15:0] pc = 0;
 
-LED7Seg led7seg(clk, seg, segsel, data);
+LED7Seg led7seg(clk, seg, segsel, pc);
 
-/*
-initial begin
-	data = 16'h1246;
+
+reg [24:0] clk_counter = 0;
+always @(posedge clk) begin
+	clk_counter = clk_counter + 1;
 end
-*/
+
+always @(posedge clk_counter[24]) begin
+	pc = pc + 1;
+end
+
 endmodule
